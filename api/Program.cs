285b3bb7c3ae0b5b066ac0a9561a58f35d281302
Yael Bakker <yv.bakker@student.han.nl>
2022-04-14 22:53:32 +0200

    Update Program.cs
    * Serilog configuration
    * DbContext & Configuration
    * Add ListingService as scoped service

--- api/Program.cs
+++ api/Program.cs
@@ -1,11 +1,29 @@
+using InsideAirbnb.Models;
+using InsideAirbnb.Services;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.UseSerilog((ctx, lc) => lc
+    .WriteTo.Console());
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<Airbnb2022Context>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("default"));
+});
+
+builder.Services.AddScoped<IListingService, ListingService>();
 
 var app = builder.Build();
 
