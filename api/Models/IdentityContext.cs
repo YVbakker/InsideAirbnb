732097be6fd2bb856aa8adb4c9ef1eef357f7d28
Yael Bakker <yv.bakker@student.han.nl>
2022-04-24 16:05:56 +0200

    Add IdentityContext, Initial migration

---
+++ api/Models/IdentityContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using InsideAirbnb.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace InsideAirbnb.Models
+{
+    public class IdentityContext : IdentityDbContext<User>
+    {
+        public IdentityContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
