ca9e7f5b7a6f69ab56d604dedcbe55ecd939fc45
Yael Bakker <yv.bakker@student.han.nl>
2022-04-24 16:03:27 +0200

    Add User model as IdentityUser

---
+++ api/Models/User.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace InsideAirbnb.Models;
+
+public class User : IdentityUser
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+}
+
+public class UserRegistrationDto
+{
+    public string? FirstName { get; init; }
+    public string? LastName { get; init; }
+
+    [Required(ErrorMessage = "Username is required")]
+    public string? UserName { get; init; }
+
+    [Required(ErrorMessage = "Password is required")]
+    public string? Password { get; init; }
+    public string? Email { get; init; }
+    public string? PhoneNumber { get; init; }
+}
+
+public class UserLoginDto 
+{
+    [Required(ErrorMessage = "Username is required")]
+    public string? UserName { get; init; }
+
+    [Required(ErrorMessage = "Password is required")]
+    public string? Password { get; init; }
+}
