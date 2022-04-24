58525b06ad818f27b4b123469055db9ada31c363
Yael Bakker <yv.bakker@student.han.nl>
2022-04-24 16:02:20 +0200

    Add mapster, jwt & identity

--- api/Program.cs
+++ api/Program.cs
@@ -1,6 +1,10 @@
+using System.Text;
 using InsideAirbnb.Models;
 using InsideAirbnb.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Prometheus;
 using Serilog;
 
@@ -14,19 +18,55 @@ builder.Host.UseSerilog((ctx, lc) => lc
     .WriteTo.Console());
 // Add services to the container.
 
+builder.Services.AddIdentity<User, IdentityRole>(i =>
+    {
+        i.Password.RequireDigit = false;
+        i.Password.RequireLowercase = false;
+        i.Password.RequireUppercase = false;
+        i.Password.RequireNonAlphanumeric = false;
+        i.User.RequireUniqueEmail = true;
+    })
+    .AddEntityFrameworkStores<IdentityContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        var config = builder.Configuration.GetSection("jwtConfig");
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = config["validIssuer"],
+            ValidAudience = config["validAudience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["secret"]))
+        };
+    });
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<AirBNBContext>(options =>
+builder.Services.AddDbContext<AirBnbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("default"));
+});
+
+builder.Services.AddDbContext<IdentityContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("identity"));
 });
 
 builder.Services.AddScoped<IListingService, ListingService>();
 builder.Services.AddScoped<INeighbourhoodService, NeighbourhoodService>();
 builder.Services.AddScoped<IReviewService, ReviewService>();
+builder.Services.AddScoped<IUserService, UserService>();
 
 var app = builder.Build();
 
@@ -39,13 +79,11 @@ if (app.Environment.IsDevelopment())
 
 app.MapMetrics();
 
-app.UseHttpMetrics(options =>
-{
-    options.ReduceStatusCodeCardinality();
-});
+app.UseHttpMetrics(options => { options.ReduceStatusCodeCardinality(); });
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
