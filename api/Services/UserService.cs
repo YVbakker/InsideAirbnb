0c55c97e4219ac879f0733e9baf5e4f169e1045d
Yael Bakker <yv.bakker@student.han.nl>
2022-05-04 21:37:40 +0200

    Refactor UserService to be stateless

--- api/Services/UserService.cs
+++ api/Services/UserService.cs
@@ -1,18 +1,15 @@
-using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
-using System.Text;
-using InsideAirbnb.Models;
+using InsideAirbnb.api.Models;
+using InsideAirbnb.common.Models;
 using Mapster;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.IdentityModel.Tokens;
 
-namespace InsideAirbnb.Services;
+namespace InsideAirbnb.api.Services;
 
 public class UserService : IUserService
 {
     private readonly UserManager<User> _userManager;
     private readonly IConfiguration _configuration;
-    private User? _user;
 
     public UserService(UserManager<User> userManager, IConfiguration configuration)
     {
@@ -25,53 +22,27 @@ public class UserService : IUserService
         var user = dto.Adapt<User>();
         return await _userManager.CreateAsync(user, dto.Password);
     }
-
-    public async Task<bool> ValidateAsync(UserLoginDto dto)
+    
+    public async Task<User?> ValidateAsync(UserLoginDto dto)
     {
-        _user = await _userManager.FindByNameAsync(dto.UserName);
-        var result = _user != null && await _userManager.CheckPasswordAsync(_user, dto.Password);
-        return result;
+        var user = await _userManager.FindByNameAsync(dto.UserName);
+        if (user is null)
+        {
+            return null;
+        }
+        var isValid = await _userManager.CheckPasswordAsync(user, dto.Password);
+        return !isValid ? null : user;
     }
 
-    public async Task<string> CreateTokenAsync()
-    {
-        var signingCredentials = GetSigningCredentials();
-        var claims = await GetClaims();
-        var tokenOptions = GenerateTokenOptions(signingCredentials, claims);
-        return new JwtSecurityTokenHandler().WriteToken(tokenOptions);
-    }
-
-    private SigningCredentials GetSigningCredentials()
-    {
-        var jwtConfig = _configuration.GetSection("jwtConfig");
-        var key = Encoding.UTF8.GetBytes(jwtConfig["Secret"]);
-        var secret = new SymmetricSecurityKey(key);
-        return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
-    }
-
-    private async Task<List<Claim>> GetClaims()
+    public async Task<List<Claim>> GetClaims(User user)
     {
         var claims = new List<Claim>
         {
-            new Claim(ClaimTypes.Name, _user.UserName)
+            new(ClaimTypes.Name, user.UserName)
         };
-        var roles = await _userManager.GetRolesAsync(_user);
+        var roles = await _userManager.GetRolesAsync(user);
         claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
 
         return claims;
-    }
-
-    private JwtSecurityToken GenerateTokenOptions(SigningCredentials signingCredentials, IEnumerable<Claim> claims)
-    {
-        var jwtSettings = _configuration.GetSection("JwtConfig");
-        var tokenOptions = new JwtSecurityToken
-        (
-            jwtSettings["validIssuer"],
-            jwtSettings["validAudience"],
-            claims,
-            expires: DateTime.Now.AddMinutes(Convert.ToDouble(jwtSettings["expiresIn"])),
-            signingCredentials: signingCredentials
-        );
-        return tokenOptions;
     }
 }
