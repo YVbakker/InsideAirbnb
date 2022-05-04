37d45ba4fcaed26c308181a8401b5b3931e0e661
Yael Bakker <yv.bakker@student.han.nl>
2022-05-04 22:17:32 +0200

    Add Listing parameter query

---
+++ api/Models/Parameters/ListingParameters.cs
@@ -0,0 +1,8 @@
+namespace InsideAirbnb.api.Models.Parameters;
+
+public class ListingParameters
+{
+    public string? Price { get; set; }
+    public string? Neighbourhood { get; set; }
+    public int? ReviewScoresRating { get; set; }
+}
--- api/Services/ListingService.cs
+++ api/Services/ListingService.cs
@@ -1,4 +1,5 @@
 using InsideAirbnb.api.Models;
+using InsideAirbnb.api.Models.Parameters;
 using InsideAirbnb.common.Utils;
 using Microsoft.EntityFrameworkCore;
 
@@ -20,5 +21,27 @@ public class ListingService : IListingService
         var query = _listings.AsQueryable();
         var paginatedList = await PaginatedList<Listing>.CreateAsync(query, pageIndex, pageSize);
         return paginatedList;
+    }
+
+    private IQueryable<Listing> CreateFiltersQuery(ListingParameters param)
+    {
+        var queryable = _listings.AsQueryable();
+        if (!string.IsNullOrEmpty(param.Price))
+        {
+            queryable = queryable.Where(l => l.Price != null && l.Price.Equals(param.Price));
+        }
+
+        if (!string.IsNullOrEmpty(param.Neighbourhood))
+        {
+            queryable = queryable.Where(l => l.Neighbourhood != null && l.Neighbourhood.Equals(param.Neighbourhood));
+        }
+
+        if (param.ReviewScoresRating.HasValue)
+        {
+            queryable = queryable.Where(l =>
+                l.ReviewScoresRating.HasValue && l.ReviewScoresRating.Equals(param.ReviewScoresRating.Value));
+        }
+
+        return queryable;
     }
 }
