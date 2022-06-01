c9b43aaf98c8f78071e71d16cf1975f09cd4482f
Yael Bakker <yv.bakker@student.han.nl>
2022-06-01 22:12:20 +0200

    Iron out docker env related bug

--- InsideAirbnb/Utils/PriceParser.cs
+++ InsideAirbnb/Utils/PriceParser.cs
@@ -4,17 +4,26 @@ namespace InsideAirbnb.Utils;
 
 public static class PriceParser
 {
+    private static NumberFormatInfo FormatInfo { get; set; }
+
+    static PriceParser()
+    {
+        FormatInfo = new NumberFormatInfo
+        {
+            CurrencySymbol = "$"
+        };
+    }
+
     public static decimal Parse(string price)
     {
         decimal parsedPrice;
         try
         {
-            parsedPrice = decimal.Parse(price, NumberStyles.Currency);
+            parsedPrice = decimal.Parse(price, NumberStyles.Currency, FormatInfo);
         }
         catch (Exception e)
         {
             Console.WriteLine(e);
-            Console.WriteLine(price);
             throw;
         }
 
